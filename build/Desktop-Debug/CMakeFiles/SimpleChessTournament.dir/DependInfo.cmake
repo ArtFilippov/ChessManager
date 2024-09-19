31e12a33a167675ad3db4475431ac1029abe333d
ArtFilippov <artfilipp0w@yandex.ru>
2024-09-19 21:48:25 +0300

    first commit

---
+++ build/Desktop-Debug/CMakeFiles/SimpleChessTournament.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "SimpleChessTournament_autogen/timestamp" "custom" "SimpleChessTournament_autogen/deps"
+  "/home/user/SimpleChessTournament/build/Desktop-Debug/SimpleChessTournament_autogen/mocs_compilation.cpp" "CMakeFiles/SimpleChessTournament.dir/SimpleChessTournament_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/SimpleChessTournament.dir/SimpleChessTournament_autogen/mocs_compilation.cpp.o.d"
+  "/home/user/SimpleChessTournament/main.cpp" "CMakeFiles/SimpleChessTournament.dir/main.cpp.o" "gcc" "CMakeFiles/SimpleChessTournament.dir/main.cpp.o.d"
+  "/home/user/SimpleChessTournament/mainwindow.cpp" "CMakeFiles/SimpleChessTournament.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/SimpleChessTournament.dir/mainwindow.cpp.o.d"
+  "/home/user/SimpleChessTournament/player.cpp" "CMakeFiles/SimpleChessTournament.dir/player.cpp.o" "gcc" "CMakeFiles/SimpleChessTournament.dir/player.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/Desktop-Debug/CMakeFiles/SimpleChessTournament.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleChessTournament_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/SimpleChessTournament_autogen.dir/ParseCache.txt"
+  "SimpleChessTournament_autogen"
+  "CMakeFiles/SimpleChessTournament.dir/SimpleChessTournament_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/SimpleChessTournament.dir/SimpleChessTournament_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/SimpleChessTournament.dir/main.cpp.o"
+  "CMakeFiles/SimpleChessTournament.dir/main.cpp.o.d"
+  "CMakeFiles/SimpleChessTournament.dir/mainwindow.cpp.o"
+  "CMakeFiles/SimpleChessTournament.dir/mainwindow.cpp.o.d"
+  "CMakeFiles/SimpleChessTournament.dir/player.cpp.o"
+  "CMakeFiles/SimpleChessTournament.dir/player.cpp.o.d"
+  "SimpleChessTournament"
+  "SimpleChessTournament.pdb"
+  "SimpleChessTournament_autogen/mocs_compilation.cpp"
+  "SimpleChessTournament_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleChessTournament.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
